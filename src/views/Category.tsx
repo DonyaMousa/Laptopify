368ff16a452a4bfafdecce66c8b258fe9de2eec7
elrakabawi <captinisaac@gmail.com>
2023-03-15 14:13:54 +0200

    Update Category.tsx

--- src/views/Category.tsx
+++ src/views/Category.tsx
@@ -8,56 +8,11 @@ import axios from 'axios'
 import useSWR from 'swr'
 
 const sortOptions = [
-  { name: 'Most Popular', href: '#', current: true },
-  { name: 'Best Rating', href: '#', current: false },
-  { name: 'Newest', href: '#', current: false },
-  { name: 'Price: Low to High', href: '#', current: false },
-  { name: 'Price: High to Low', href: '#', current: false },
+  { name: 'الأكثر شهرة', href: '#', current: true },
+  { name: 'أعلي تقييم', href: '#', current: false },
+  { name: 'السعر: من الأدنى الى الأعلى', href: '#', current: false },
+  { name: 'السعر: من الأعلى الى الأدنى', href: '#', current: false },
 ]
-// const subCategories = [
-//   { name: 'Totes', href: '#' },
-//   { name: 'Backpacks', href: '#' },
-//   { name: 'Travel Bags', href: '#' },
-//   { name: 'Hip Bags', href: '#' },
-//   { name: 'Laptop Sleeves', href: '#' },
-// ]
-// const filters = [
-//   {
-//     id: 'color',
-//     name: 'Size of Item',
-//     options: [
-//       { value: 'white', label: 'White', checked: false },
-//       { value: 'beige', label: 'Beige', checked: false },
-//       { value: 'blue', label: 'Blue', checked: true },
-//       { value: 'brown', label: 'Brown', checked: false },
-//       { value: 'green', label: 'Green', checked: false },
-//       { value: 'purple', label: 'Purple', checked: false },
-//     ],
-//   },
-//   {
-//     id: 'category',
-//     name: 'Category',
-//     options: [
-//       { value: 'new-arrivals', label: 'New Arrivals', checked: false },
-//       { value: 'sale', label: 'Sale', checked: false },
-//       { value: 'travel', label: 'Travel', checked: true },
-//       { value: 'organization', label: 'Organization', checked: false },
-//       { value: 'accessories', label: 'Accessories', checked: false },
-//     ],
-//   },
-//   {
-//     id: 'size',
-//     name: 'Size',
-//     options: [
-//       { value: '2l', label: '2L', checked: false },
-//       { value: '6l', label: '6L', checked: false },
-//       { value: '12l', label: '12L', checked: false },
-//       { value: '18l', label: '18L', checked: false },
-//       { value: '20l', label: '20L', checked: false },
-//       { value: '40l', label: '40L', checked: true },
-//     ],
-//   },
-// ]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -140,7 +95,94 @@ const Category: FC<any> = () => {
         <div className="bg-white">
         <div>
             {/* Mobile filter dialog */}
+            <Transition.Root show={mobileFiltersOpen} as={Fragment}>
+                <Dialog as="div" className="relative z-40 lg:hidden" onClose={setMobileFiltersOpen}>
+                    <Transition.Child
+                    as={Fragment}
+                    enter="transition-opacity ease-linear duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="transition-opacity ease-linear duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                    >
+                    <div className="fixed inset-0 bg-black bg-opacity-25" />
+                    </Transition.Child>
 
+                    <div className="fixed inset-0 z-40 flex">
+                    <Transition.Child
+                        as={Fragment}
+                        enter="transition ease-in-out duration-300 transform"
+                        enterFrom="translate-x-full"
+                        enterTo="translate-x-0"
+                        leave="transition ease-in-out duration-300 transform"
+                        leaveFrom="translate-x-0"
+                        leaveTo="translate-x-full"
+                    >
+                        <Dialog.Panel className="relative ml-auto pr-5 flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
+                        <div className="flex items-center justify-between px-4">
+                            <h2 className="text-lg font-medium text-gray-900">Filters</h2>
+                            <button
+                            type="button"
+                            className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
+                            onClick={() => setMobileFiltersOpen(false)}
+                            >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                        </div>
+
+                        {/* Filters */}
+                        <form className="mt-4 border-t border-gray-200">
+                        {filtersKeys.map((section) => (
+                            <Disclosure as="div" key={section.id} className="border-b border-gray-200 py-6">
+                                {({ open }) => (
+                                <>
+                                    <h3 className="-my-3 flow-root">
+                                    <Disclosure.Button className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                                        <span className="font-medium text-gray-900">{section.name}</span>
+                                        <span className="ml-6 flex items-center">
+                                        {open ? (
+                                            <MinusIcon className="h-5 w-5" aria-hidden="true" />
+                                        ) : (
+                                            <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                                        )}
+                                        </span>
+                                    </Disclosure.Button>
+                                    </h3>
+                                    <Disclosure.Panel className="pt-6">
+                                    <div className="space-y-4">
+                                        {section.options.map((option, optionIdx) => (
+                                        <div key={option} className="flex items-center">
+                                            <input
+                                            id={`filter-${section.id}-${optionIdx}`}
+                                            name={`${section.id}[]`}
+                                            defaultValue={option}
+                                            type="checkbox"
+                                            defaultChecked={selectedFilters?.includes(option)}
+                                            onClick={() => {handleFilter(option)}}
+                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                            />
+                                                <label
+                                                htmlFor={`filter-${section.id}-${optionIdx}`}
+                                                className="mr-3 text-sm text-gray-600"
+                                                >
+                                                    {option}
+                                                </label>
+                                        </div>
+                                        ))}
+                                    </div>
+                                    </Disclosure.Panel>
+                                </>
+                                )}
+                            </Disclosure>
+                            ))}
+                        </form>
+                        </Dialog.Panel>
+                    </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition.Root>
             <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex items-baseline justify-between border-b border-gray-200 py-6">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900">{category ? categories[category] : 'Tablebt'}</h1>
@@ -167,7 +209,7 @@ const Category: FC<any> = () => {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                     >
-                    <Menu.Items className="absolute -right-[70px] z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute -right-[100px] z-10 mt-2 w-48 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <div className="py-1">
                         {sortOptions.map((option) => (
                             <Menu.Item key={option.name}>
@@ -175,7 +217,7 @@ const Category: FC<any> = () => {
                                 <a
                                 href={option.href}
                                 className={classNames(
-                                    option.current ? 'font-medium text-gray-900' : 'text-gray-500',
+                                    option.current ? 'font-medium text-gray-900 text-right' : 'text-gray-500 text-right',
                                     active ? 'bg-gray-100' : '',
                                     'block px-4 py-2 text-sm'
                                 )}
@@ -190,10 +232,6 @@ const Category: FC<any> = () => {
                     </Transition>
                 </Menu>
 
-                {/* <button type="button" className="-m-2 ml-5 p-2 text-gray-400 hover:text-gray-500 sm:ml-7">
-                    <span className="sr-only">View grid</span>
-                    <Squares2X2Icon className="h-5 w-5" aria-hidden="true" />
-                </button> */}
                 <button
                     type="button"
                     className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
@@ -275,13 +313,13 @@ const Category: FC<any> = () => {
                             </div> :
 
 
-                        <div className="grid grid-cols-3 gap-2">
+                        <div className="grid grid-cols-1 gap-2">
                         {filteredProducts && filteredProducts.map((product: any, i: number) => (    
-                            <div key={i} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                                <a href="#">
-                                    <img className="p-8 rounded-t-lg" src={product.image} alt="product image" />
-                                </a>
-                                <div className="px-5 pb-5">
+                            <div key={i} className="w-full flex flex-row p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                <div className="basis-1/4">
+                                    <img width={300} className="p-8 rounded-t-lg" src={product.image} alt="product image" />
+                                </div>
+                                <div className="px-5 py-6 basis-3/4">
                                     <a href="#">
                                         <h5 onClick={() => navigate(`/product/${category}/${product._id}`)} className="text-lg font-normal tracking-tight text-gray-900 dark:text-white hover:underline cursor-pointer">{product.title}</h5>
                                     </a>
@@ -295,6 +333,7 @@ const Category: FC<any> = () => {
                                     )}
                                     <div className="flex items-center justify-between">
                                         <span className="text-3xl font-bold text-gray-900 dark:text-white">{product.price}</span>
+                                        <a onClick={() => navigate(`/product/${category}/${product._id}`)} className="text-black cursor-pointer bg-white shadow-2xl hover:text-gray-500 hover:bg-gray-100 ring-1 ring-black ring-opacity-5 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">المزيد عن المنتج</a>
                                     </div>
                                 </div>
                             </div>
